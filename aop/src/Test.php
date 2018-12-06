8178a10cb551762e9fcfdba929be7228c51e617b
dadyday <dadyday@localhost>
2018-12-06 08:58:43 +0100

    proxy removed

--- aop/src/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Test;
-
-class Test__AopProxied {
-	function doWhat($what = 'nothing') {
-		echo 'im doing ...'.$what;
-	}
-}
-include_once AOP_CACHE_DIR . '/_proxies/src/Test.php/Test/Test.php';
